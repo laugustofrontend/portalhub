6c408655464078b7d9a4f8392b4545dd209c87ff
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-05-25 17:16:44 -0300

    adicionado chart em analise de carteira

--- src/assets/js/pages/morris.init.js
+++ src/assets/js/pages/morris.init.js
@@ -10,6 +10,21 @@
 
   var MorrisCharts = function() {};
 
+  //creates Bar chart
+  MorrisCharts.prototype.createBarChart  = function(element, data, xkey, ykeys, labels, lineColors) {
+    Morris.Bar({
+      element: element,
+      data: data,
+      xkey: xkey,
+      ykeys: ykeys,
+      labels: labels,
+      hideHover: 'auto',
+      resize: true, //defaulted to true
+      gridLineColor: '#3d4853',
+      barColors: lineColors
+    });
+  },
+
   //creates line chart
   MorrisCharts.prototype.createDonutChart = function(element, data, colors) {
     Morris.Donut({
@@ -22,12 +37,25 @@
 },
   MorrisCharts.prototype.init = function() {
     //creating donut chart
-    var $donutData = [
+    const $donutData = [
       {label: "Download Sales", value: 12},
       {label: "In-Store Sales", value: 30},
       {label: "Mail-Order Sales", value: 20}
     ];
     this.createDonutChart('morris-donut-example', $donutData, ["#3bafda", "#ededed", "#80deea"]);
+
+    //creating bar chart
+    const $barData  = [
+      { y: 'Ações', a: 20, b: 10},
+      { y: 'Opções', a: 15,  b: 25},
+      { y: 'Renda Fixa', a: 5,  b: 15},
+      { y: 'Funda de Investimento', a: 100,  b: 25},
+      { y: 'Mercado Futuro(BMF)', a: 10,  b: 2},
+      { y: 'Tesouro Direto', a: 67,  b: 25},
+      { y: 'COE', a: 5,  b: 10},
+      { y: 'Clube de Investimento', a: 12,  b: 0}
+    ];
+    this.createBarChart('morris-bar-example', $barData, 'y', ['a', 'b'], ['','',''], ["#017bc4", "#278070"]);
   },
 
   //init
@@ -36,6 +64,6 @@
 
 //initializing
 function($) {
-    "use strict";
-    $.MorrisCharts.init();
+  "use strict";
+  $.MorrisCharts.init();
 }(window.jQuery);
